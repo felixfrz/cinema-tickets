e25d26327a2a89d15211ff1b55deb35bd1b30746
Ayodeji Felix Oladoyin <oladoyinayodeji@gmail.com>
2025-02-12 11:57:41 +0000

    Set Up Test Environment by Installing Jest Module

--- src/pairtest/TicketService.js
+++ src/pairtest/TicketService.js
@@ -1,10 +1,25 @@
 import TicketTypeRequest from './lib/TicketTypeRequest.js';
 import InvalidPurchaseException from './lib/InvalidPurchaseException.js';
+import TicketPaymentService from '../thirdparty/paymentgateway/TicketPaymentService.js'
+import SeatReservationService from '../thirdparty/seatbooking/SeatReservationService.js'
 
 export default class TicketService {
   /**
    * Should only have private methods other than the one below.
    */
+  #paymentService;
+  #reservationService;
+  #TICKET_PRICE = {
+    INFANT: 0,
+    CHILD: 15,
+    ADULT:25
+  }
+  MAX_TICKETS = 25;
+
+  constructor() {
+    this.#paymentService = new TicketPaymentService();
+    this.#reservationService = new SeatReservationService();
+  }
 
   purchaseTickets(accountId, ...ticketTypeRequests) {
     // throws InvalidPurchaseException
